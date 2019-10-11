74a1389c17bedc91a0968ee75ac3f29c368e3835
Aidan Reilly <areilly@tycoint.com>
2019-10-11 13:40:18 +0100

    init commit of crow pd implementation

---
+++ crowpd.lua
@@ -0,0 +1,114 @@
+ofelia.inlets  = 2
+ofelia.outlets = 3
+
+
+--- Marshall returned serial stream
+-- messages returned from serial port
+local cmd_bytes  = {} -- temporary space to construct the received string
+local cmd_length = -1
+local cmd_rxd    = 0
+function from( byte )
+	table.insert( cmd_bytes, byte )
+	cmd_rxd = cmd_rxd + 1
+	if cmd_rxd == cmd_length then
+		if cmd_bytes[#cmd_bytes] == 13 then -- check for carriage return
+			eval( bytes_to_string( cmd_bytes))
+			cmd_bytes  = {}
+		end
+		cmd_length = -1
+		cmd_rxd    = 0
+    end
+end
+
+function from_length( n )
+	if n ~= 0 then
+		cmd_length = n
+	end
+end
+
+function eval( str )
+	if #str == 0 then return end
+	local split = string.find( str, "\r" )
+	local now = str:sub( 1, split)
+	if string.find( now, "^%^%^") then
+		pcall( loadstring( now:sub(3)))
+    else
+        to_pd_print( now )
+    end
+	if split ~= nil then --in case of dropped \r
+		eval( str:sub( split+1))
+	end
+end
+
+--- Request/callback pairs
+-- from pd
+function get_input( channel )
+    tell_crow( "get_cv("..channel..")")
+end
+function get_output( channel )
+    tell_crow( "get_out("..channel..")")
+end
+-- continues to ->
+function stream( channel, value )
+    to_pd("stream", channel, value)
+end
+
+function change( channel, state )
+    to_pd("change", channel, state )
+end
+
+function output( channel, state )
+    to_pd("output", channel, state )
+end
+
+function midi( ... )
+    to_pd("midi", ...)
+end
+
+function version( ... )
+    to_pd("version", ...)
+end
+
+function identity( ... )
+    to_pd("identity", ...)
+end
+
+--- Helper conversion functions
+
+function tell_crow( str )
+	if string.find( str, "^%^%^" ) then -- 3char command
+		outlet(0, string_to_serial( string.sub( str, 1, 3)))
+	elseif string.len(str) >= 64 then -- multi-line codeblock
+		-- TODO if more than 1kB, need to split into multiple messages
+		outlet(0, string_to_serial( '```'))
+		outlet(0, string_to_serial( str))
+		outlet(0, string_to_serial( '```'))
+	else -- standard code block
+		outlet(0, string_to_serial( str))
+	end
+end
+
+function string_to_serial( str )
+    ascii = {}
+    for c in str:gmatch"." do
+        table.insert(ascii, c:byte())
+    end
+	table.insert(ascii, string.byte'\n')
+    return ascii
+end
+
+function to_pd( ... )
+    outlet(1, ...)
+end
+
+function to_pd_print( ... )
+    outlet(2, ...)
+end
+
+function bytes_to_string( bytes )
+    local str = ""
+    for i=1, #bytes do
+        str = str .. string.char(bytes[i])
+    end
+	return str
+end
